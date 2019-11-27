6177ec9c0aa04bc4514cbfeb75d342cdcc05888a
P. Christeas <xrg@pefnos.com>
2019-11-27 22:56:06 +0200

    junit: fix when failed step has no traceback lines

--- behave/reporter/junit.py
+++ behave/reporter/junit.py
@@ -411,8 +411,11 @@ class JUnitReporter(Reporter):
                     failure_type = scenario.exception.__class__.__name__
                 failure.set(u'type', failure_type)
                 failure.set(u'message', scenario.error_message or "")
-                traceback_lines = [u"Traceback:\n"] + scenario.exc_traceback
-                text = _text(u"".join(traceback_lines))
+                if scenario.exc_traceback:
+                    traceback_lines = [u"Traceback:\n"] + scenario.exc_traceback
+                    text = _text(u"".join(traceback_lines))
+                else:
+                    text = u''
             failure.append(CDATA(text))
             case.append(failure)
         elif (scenario.status in (Status.skipped, Status.untested)
